c0c46979b6cca088ff9aeca538cb874a58a5fbb0
Kar <2478084@cognizant.com>
2026-04-21 14:06:22 +0530

    pushed compliance-service

---
+++ compliance-service/src/main/java/com/cognizant/agriserve/complianceservice/dto/response/ComplianceRecordResponseDTO.java
@@ -0,0 +1,33 @@
+package com.cognizant.agriserve.complianceservice.dto.response;
+
+import com.cognizant.agriserve.complianceservice.entity.ComplianceRecord.ComplianceType; // Updated import
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ComplianceRecordResponseDTO {
+
+    private Long complianceId;
+
+    private Long entityId;
+
+//    private String entityName;
+
+    private ComplianceType type;
+
+    private Long officerId;
+
+    private String officerName;
+
+    private String result;
+
+    private LocalDateTime date;
+
+    private String notes;
+
+}
